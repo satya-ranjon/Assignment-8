ee6d7e50292c43aa5d3bcf567b14b973aa9f499a
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-09-25 04:53:49 +0600

    feat: Configure React Router routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import AppWrapper from "../layout/AppWrapper";
+import Home from "../pages/Home";
+import Donation from "../pages/Donation";
+import DonationDetails from "../pages/DonationDetails";
+import Statistics from "../pages/Statistics";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppWrapper />,
     errorElement: <ErrorPage />,
-    children: [{}],
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/donation",
+        element: <Donation />,
+      },
+      {
+        path: "/donation/:donationID",
+        element: <DonationDetails />,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics />,
+      },
+    ],
   },
 ]);
 
