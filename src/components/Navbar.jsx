6482062571a1508920f6e4926fc37aaf798ea11c
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-09-25 04:50:56 +0600

    feat: Add responsive navigation bar component
    This commit introduces a responsive navigation bar component using React Router. Key features and changes include:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,89 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const navData = [
+  { link: "/", name: "Home" },
+  { link: "/donation", name: "Donation" },
+  { link: "/statistics", name: "Statistics" },
+];
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const handleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="container mx-auto px-5 sm:px-10 py-3 sm:py-5">
+      <div className="flex justify-between items-center">
+        <div>
+          <img
+            src="https://i.ibb.co/DKLBbcd/Logo.png"
+            alt="Logo"
+            className="w-52 h-12 lg:w-60	lg:h-16 hidden sm:block"
+          />
+          <img
+            src="https://i.ibb.co/HHQBmsM/Group.png"
+            alt="Group"
+            className="sm:hidden w-10 h-10"
+          />
+        </div>
+        <div className="sm:hidden cursor-pointer" onClick={handleNavbar}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+
+        {isOpen && (
+          <ul
+            className={`sm:hidden absolute right-0 top-16 flex flex-col gap-5 justify-center items-center py-6 w-full bg-white`}>
+            {navData.map((item) => (
+              <NavLink
+                key={item.link}
+                to={item.link}
+                onClick={handleNavbar}
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "text-red-500 font-bold border-b-2 border-b-red-500"
+                      : "font-normal"
+                  } text-lg `
+                }>
+                {item.name}
+              </NavLink>
+            ))}
+          </ul>
+        )}
+
+        <ul className={`hidden sm:flex justify-start items-center gap-10 `}>
+          {navData.map((item) => (
+            <NavLink
+              key={item.link}
+              to={item.link}
+              onClick={handleNavbar}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "text-red-500 font-bold border-b-2 border-b-red-500"
+                    : "font-normal"
+                } text-lg `
+              }>
+              {item.name}
+            </NavLink>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
