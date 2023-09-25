cba40032efaef66430f58101b34397983612a2ed
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-09-25 23:12:19 +0600

    feat: Implement search functionality in Home component
    In this commit, I've added search functionality to the Home component:
    
    - Introduced state variables 'searchQuery' and 'searchResult'.
    - Implemented the 'handleSearch' function to filter donations by category.
    - Updated the rendering logic to display search results when available.

--- src/pages/home/Card.jsx
+++ src/pages/home/Card.jsx
@@ -2,21 +2,22 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const Card = ({ donate }) => {
-  const { id, title, img, colors, tag } = donate || {};
+  const { id, title, picture, category_bg, category, card_bg, text_btn_bg } =
+    donate || {};
 
   return (
-    <Link to={`/donation/${id}`}>
+    <Link to={`/donation-details/${id}`}>
       <div
-        style={{ backgroundColor: colors[2] }}
+        style={{ backgroundColor: card_bg }}
         className={`flex flex-col gap-4 rounded-md overflow-hidden cursor-pointer lg:h-[23.2rem]`}>
-        <img src={img} alt={title} className="w-full h-56" />
+        <img src={picture} alt={title} className="w-full h-56" />
         <div
-          style={{ backgroundColor: colors[1], color: colors[0] }}
+          style={{ backgroundColor: category_bg, color: text_btn_bg }}
           className={`px-4 py-2 w-fit mx-4 font-semibold`}>
-          {tag}
+          {category}
         </div>
         <h1
-          style={{ color: colors[0] }}
+          style={{ color: text_btn_bg }}
           className={`mx-4 mb-4 font-bold text-2xl`}>
           {title}
         </h1>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,33 +1,81 @@
 import { useLoaderData } from "react-router-dom";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
   const data = useLoaderData();
+
+  const handleSearch = () => {
+    const searchResult = data.filter((item) =>
+      item.category.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setSearchResult(searchResult);
+  };
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setSearchResult([]);
+    }
+  }, [searchQuery]);
+
   return (
-    <div>
-      {/* hero section  */}
-      <div className="absolute top-32 sm:top-52 left-0 right-0 w-full flex flex-col justify-center items-center">
-        <h1 className="font-bold text-lg sm:text-4xl lg:text-5xl	">
-          I Grow By Helping People In Need
-        </h1>
-        <div className="flex justify-start items-center mt-8 rounded-lg overflow-hidden ">
-          <input
-            type="text"
-            placeholder="Search here..."
-            className="text-lg outline-none p-1 sm:p-2 w-52 sm:w-full border-[1px] border-gray-200 border-r-0 rounded-l-lg placeholder:text-xs"
-          />
-          <button className="p-1 sm:p-2 bg-red-500 text-lg text-white px-3 sm:px-5  border-[1px] border-red-500">
-            Search
-          </button>
+    <>
+      <div className=" h-80 sm:h-[478px] bg-[url('https://i.ibb.co/CBP7p28/Rectangle-4281.png')] w-full  bg-cover ">
+        <div className="flex justify-center items-center h-full w-full bg-[#ffffffeb] ">
+          <div className="w-full flex flex-col justify-center items-center ">
+            <h1 className="font-bold text-lg sm:text-4xl lg:text-5xl">
+              I Grow By Helping People In Need
+            </h1>
+            <div className="flex justify-start items-center mt-8 rounded-lg overflow-hidden ">
+              <input
+                type="text"
+                placeholder="Search here..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="text-lg outline-none p-1 sm:p-2 w-52 sm:w-full border-[1px] border-gray-200 border-r-0 rounded-l-lg placeholder:text-xs"
+              />
+              <button
+                onClick={handleSearch}
+                className="p-1 sm:p-2 bg-red-500 text-lg text-white px-3 sm:px-5  border-[1px] border-red-500">
+                Search
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:lg:grid-cols-4 gap-4 my-12 ">
-        {data?.map((donate) => (
-          <Card key={donate.id} donate={donate} />
-        ))}
+      {/* hero section  */}
+
+      <div className="container mx-auto px-5 sm:px-10">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 my-12 ">
+          {searchResult?.length > 0
+            ? searchResult.map((donate) => (
+                <Card key={donate.id} donate={donate} />
+              ))
+            : data?.map((donate) => <Card key={donate.id} donate={donate} />)}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default Home;
+
+{
+  /* <div className="absolute top-32 sm:top-52 left-0 right-0 w-full flex flex-col justify-center items-center z-10">
+  <h1 className="font-bold text-lg sm:text-4xl lg:text-5xl	">
+    I Grow By Helping People In Need
+  </h1>
+  <div className="flex justify-start items-center mt-8 rounded-lg overflow-hidden ">
+    <input
+      type="text"
+      placeholder="Search here..."
+      className="text-lg outline-none p-1 sm:p-2 w-52 sm:w-full border-[1px] border-gray-200 border-r-0 rounded-l-lg placeholder:text-xs"
+    />
+    <button className="p-1 sm:p-2 bg-red-500 text-lg text-white px-3 sm:px-5  border-[1px] border-red-500">
+      Search
+    </button>
+  </div>
+</div>; */
+}
